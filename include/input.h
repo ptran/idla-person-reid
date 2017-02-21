20d21c464851bd3565027ec88c40c8d636ec6e64
Philip Tran <ptran516@gmail.com>
2017-02-20 21:18:58 -0500

    Fix input layer for rgb pairs.

--- include/input.h
+++ include/input.h
@@ -13,7 +13,7 @@
 class input_rgb_image_pair {
 public:
     typedef dlib::matrix<dlib::rgb_pixel> image_type;
-    typedef std::pair<image_type*,image_type*> input_type;
+    typedef std::pair<const image_type*,const image_type*> input_type;
 
     /*!
         This function converts input image pairs into a data tensor.
@@ -49,10 +49,12 @@ void input_rgb_image_pair::to_tensor(
 
     // Set data tensor size
     const long nr = (*ibegin->first).nr();
-    const long nc = (*ibegin->first).nr();
+    const long nc = (*ibegin->first).nc();
     data.set_size(std::distance(ibegin, iend)*2, 3, nr, nc);
 
-    long offset = nr*nc*3;
+    long channel_offset = nr*nc;
+    long image_offset = 3*channel_offset;
+
     float* data_ptr = data.host();
     for (auto i = ibegin; i != iend; ++i) {
         DLIB_CASSERT((*i->first).nc() == nc && (*i->second).nc() == nc &&
@@ -75,20 +77,24 @@ void input_rgb_image_pair::to_tensor(
         // Populate data tensor
         for (long r = 0; r < nr; ++r) {
             for (long c = 0; c < nc; ++c) {
-                // Copy the data pointer while also iterating to the next
-                // element.
+                // Copy the data pointer
                 float* p = data_ptr++;
-                *p = (static_cast<float>((*i->first)(r,c).red)-stats1.mean())/(stats1.stddev()+1e-7);
-                *(p+offset) = (static_cast<float>((*i->second)(r,c).red)-stats2.mean())/(stats2.stddev()+1e-7);
-                ++p;
-                *p = (static_cast<float>((*i->first)(r,c).green)-stats1.mean())/(stats1.stddev()+1e-7);
-                *(p+offset) = (static_cast<float>((*i->second)(r,c).green)-stats2.mean())/(stats2.stddev()+1e-7);
-                ++p;
-                *p = (static_cast<float>((*i->first)(r,c).blue)-stats1.mean())/(stats1.stddev()+1e-7);
-                *(p+offset) = (static_cast<float>((*i->second)(r,c).blue)-stats2.mean())/(stats2.stddev()+1e-7);
+                dlib::rgb_pixel tmp1 = (*i->first)(r,c);
+                dlib::rgb_pixel tmp2 = (*i->second)(r,c);
+
+                *p = (static_cast<float>(tmp1.red)-stats1.mean())/(stats1.stddev()+1e-7);
+                *(p+image_offset) = (static_cast<float>(tmp2.red)-stats2.mean())/(stats2.stddev()+1e-7);
+                p += channel_offset;
+
+                *p = (static_cast<float>(tmp1.green)-stats1.mean())/(stats1.stddev()+1e-7);
+                *(p+image_offset) = (static_cast<float>(tmp2.green)-stats2.mean())/(stats2.stddev()+1e-7);
+                p += channel_offset;
+
+                *p = (static_cast<float>(tmp1.blue)-stats1.mean())/(stats1.stddev()+1e-7);
+                *(p+image_offset) = (static_cast<float>(tmp2.blue)-stats2.mean())/(stats2.stddev()+1e-7);
             }
         }
-        data_ptr += offset;
+        data_ptr += 5*channel_offset;
     }
 }
 
